5e3ee1dc822a0eec6b6833ea244d8be53330fcc1
Carlos Aldana <carlos.aldana@gmail.com>
2024-12-17 12:34:27 -0500

    chnages

--- Web/src/environments/environment.prod.ts
+++ Web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: "EscolarAdminv1.0",
+  appVersion: "TestWeb1.0",
   URL_BACKEND: 'http://127.0.0.1:8000/api',
   URL_FROTEND: 'http://localhost:4200',
 };
